c0637d7a8684674979b53c29c2c8f80d8552d8f9
khaldounalhalabi <khaldoun1222@hotmail.com>
2022-08-11 14:05:25 +0300

    forgot controller

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -47,7 +47,7 @@ class ForgotController extends Controller
             //Send Email
             $user = User::where('email' , $email)->get()->first() ;
             //dd($user) ;
-            Mail::to('khaldoun@email.com')->send(new ResetPassword($user->name));
+            Mail::to($request->email)->send(new ResetPassword($user->name));
             return response()->json([
             'message' => 'Check Your Email'
         ]) ;
