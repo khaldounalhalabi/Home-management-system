3cb7b865ac3ea6913e0f429cacdff90f3104dd96
khaldounalhalabi <khaldoun1222@hotmail.com>
2022-08-11 14:03:26 +0300

    forgot controller

--- app/Mail/ResetPassword.php
+++ app/Mail/ResetPassword.php
@@ -16,9 +16,9 @@ class ResetPassword extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($name)
     {
-        //
+        $this->name = $name ; 
     }
 
     /**
@@ -29,10 +29,10 @@ class ResetPassword extends Mailable
     public function build()
     {
         return $this->from('Home-EMS@Email.com', 'Home Management System')
-        ->greeting('Hello!')
+            ->greeting('Hello'.$name)
             ->line('
-            You Want to reset your password
-            Click on the URL bellow')
+                You Want to reset your password
+                Click on the URL bellow')
             ->action('RESET YOUR PASSWORD', 'http://127.0.0.2/api/reset')
             ->line('Thank you for using our system !');
     }
