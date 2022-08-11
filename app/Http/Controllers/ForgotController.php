0c332d5104e78c5a1e7ad55366fe21967b08c4d0
khaldounalhalabi <khaldoun1222@hotmail.com>
2022-08-11 13:45:48 +0300

    forgot controller

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -15,23 +15,38 @@ use Illuminate\Validation\Rules\Password;
 
 class ForgotController extends Controller
 {
-    public function forgot(ForgotRequest $request)
+    public function forgot(Request $request)
     {
-        $email = $request->input('email') ;
+        $rules =  [
+            'email' => 'required|email' ,
+        ];
+
+
+        $validator = Validator::make($request->only( 'email'), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()]);
+        }
+
+
+        $email = $request->email ;
+
+
         if(User::where('email' , $email)->doesntExist()){
             return response()->json(['message'=>'User is not exist']) ;
         }
 
        try{
-        $token = Str::random(10) ;
+        // $token = Str::random(10) ;
 
-        DB::table('password_resets')->insert([
-            'email'=>$email ,
-            'token'=>$token ,
-        ]) ;
+        // DB::table('password_resets')->insert([
+        //     'email'=>$email ,
+        //     'token'=>$token ,
+        // ]) ;
 
             //Send Email
-            Mail::to($email)->send(new ResetPassword());
+            $user = User::where('email' , $email) ;
+            Mail::to($user)->send(new ResetPassword());
             return response()->json([
             'message' => 'Check Your Email'
         ]) ;
@@ -70,7 +85,7 @@ class ForgotController extends Controller
         catch(\Exception  $e){
             return response()->json([
                 'message' => 'there is been error' ,
-                'error' => $e->getMessage() , 
+                'error' => $e->getMessage() ,
             ]) ;
         }
 
