a2fbffe0aa2cf806b9fa4b2b29eff8a984cecd1b
khaldounalhalabi <khaldoun1222@hotmail.com>
2022-08-12 16:14:08 +0300

    time config

--- routes/api.php
+++ routes/api.php
@@ -87,7 +87,7 @@ Route::get('notification/over_consumption_peak', 'App\Http\Controllers\Notificat
 Route::get('/' , function () {
     return response()->json(
         [
-            'time' => Carbon::now("+03:00")->format('hh:mm')
+            'time' => Carbon::now("+03:00")->format('h:m')
         ]
     ) ;
 }) ;
